3a469a9e28a1b980f356202296caac0d88b93f52
Eun Oh <bdohhhhh@gmail.com>
2025-08-21 09:24:43 +0900

    add

--- second.c
+++ second.c
@@ -1,1 +1,16 @@
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int main() {
+  printf("hello\n");
+
+  // 반환값을 변수에 저장하고 사용
+  int length = strlen("문자열");
+  printf("문자열길이: %d\n", length);
+
+  double result = sqrt(16);
+  printf("16의 제곱근: %.1f\n", result);
+
+  return 0;
+}
